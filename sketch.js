62de06ae94efc3d34ec2dfc2434b7e7690549276
mokshaGodha <68106669+mokshaGodha@users.noreply.github.com>
2020-09-24 15:06:00 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -94,7 +94,7 @@ function keyPressed(){
     }
 }
 async function getBackgroundImage(){
-    var response = await fetch ("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+    var response = await fetch ("https://worldtimeapi.org/api/timezone/Asia/Kolkata");
     var data = await response.json();
     console.log(data);
     var dateTime= data.datetime;
